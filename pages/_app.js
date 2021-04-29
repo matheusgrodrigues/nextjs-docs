45bdcd29e991df751c5bb2d2849db817292a96fa
matheus <matheus.rodrigues@agenciamop.com.br>
2021-04-28 21:21:10 -0300

    Nextjs docs

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "./sass/style.scss";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+import styles from "../../components/layout.module.css";
+
+const firstPost = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Pagina do post</title>
+      </Head>
+
+      <div className={styles.container}>
+        <Image
+          src="/images/profile.jpg"
+          width="1920"
+          height="500"
+          alt="Component Image Next"
+        ></Image>
+        <Link href="/">Voltar para a pagina inicial</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default firstPost;
